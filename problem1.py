6d9c11b7e0d0635966d04d7d11ca1c7be0870148
Poojarrrr <53255528+Poojarrrr@users.noreply.github.com>
2019-10-30 23:55:48 +0530

    Add files via upload

---
+++ problem1.py
@@ -0,0 +1,6 @@
+
+character = input("Enter Name: ")
+age = int(input("Enter age: "))
+turn = int( 100 - age + 2019)
+statement =  f"Hey {character} ! You'll turn 100 years old in {turn} "
+print(statement)
